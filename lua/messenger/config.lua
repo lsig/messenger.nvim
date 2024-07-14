2570ee4f6401bf276d63532bbdacc403343d44f1
Lsig <logis21@ru.is>
2024-07-14 00:37:28 +0000

    feat: Add a type for config options

--- lua/messenger/config.lua
+++ lua/messenger/config.lua
@@ -1,17 +1,18 @@
 local M = {}
 
-M.options = nil
-
+---@class MessengerOptions
 local defaults = {
   window = {
     border = "none",
   },
 }
 
+---@class MessengerOptions
+M.options = nil
+
 function M.setup(options)
   M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
   vim.cmd("highlight MessengerHeadings guifg=#89b4fa")
-  -- vim.api.nvim_create_user_command("MessengerShow", view.messenger, {})
 end
 
 return M
